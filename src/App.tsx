458e6708ebac81b932df49e52daf45073b771cb8
BOO-0-K <boo970328@gmail.com>
2024-11-19 13:25:09 +0900

    [ADD] Drag and Drop

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useDrag, useDrop } from "react-dnd";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 480px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Layer = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(1, 1fr);
+`;
+
+const Board = styled.div`
+  padding: 20px 10px;
+  padding-top: 30px;
+  background-color: ${(props) => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 200px;
+`;
+
+const Card = styled.div`
+  border-radius: 5px;
+  margin-bottom: 5px;
+  padding: 10px 10px;
+  background-color: ${(props) => props.theme.cardColor};
+`;
+
+const toDos = ["a", "b", "c", "d", "e", "f"];
+
+const ItemType = {
+  CARD: "card",
+};
+
+interface IDraggableCardProps {
+  toDo: string;
+  index: number;
+}
+
+interface IDroppableBoardProps {
+  toDos: string[];
+}
+
+function DraggableCard({ toDo, index }: IDraggableCardProps) {
+  const [_, ref] = useDrag({
+    type: ItemType.CARD,
+    item: { index },
+  });
+
+  return (
+    <Card ref={ref}>
+      {toDo}
+    </Card>
+  );
+}
+
+function DroppableBoard({ toDos }: IDroppableBoardProps) {
+  const [_, ref] = useDrop({
+    accept: ItemType.CARD,
+  });
+
+  return (
+    <Board ref={ref}>
+      {
+        toDos.map((toDo, index) => (
+          <DraggableCard key={index} toDo={toDo} index={index} />
+        ))
+      }
+    </Board>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Layer>
+        <DroppableBoard toDos={toDos} />
+      </Layer>
+    </Wrapper>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { theme } from "./theme";
+import { Reset } from "styled-reset";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    font-weight: 300;
+    font-family: 'Source Sans Pro', sans-serif;
+    background-color: ${(props) => props.theme.bgColor};
+    color: black;
+    line-height: 1.2;
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Reset />
+      <GlobalStyle />
+      <DndProvider backend={HTML5Backend}>
+        <App />
+      </DndProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
