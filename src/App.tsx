1d3829eed0fc2042aaa5048bf1af856c64eed781
BOO-0-K <boo970328@gmail.com>
2024-11-26 14:27:19 +0900

    [ADD] Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,92 +1,34 @@
-import styled from "styled-components";
-import DroppableBoard from "./components/DroppableBoard";
-import { useRecoilState } from "recoil";
-import { toDoState } from "./atoms";
-import { useForm } from "react-hook-form";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./routes/Home";
 
-const Wrapper = styled.div`
-  display: flex;
-  max-width: 480px;
-  width: 100%;
-  margin: 0 auto;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-`;
-
-const Layer = styled.div`
-  display: grid;
-  width: 100%;
-  gap: 10px;
-  grid-template-columns: repeat(3, 1fr);
-`;
-
-const Board = styled.div`
-  padding: 20px 10px;
-  background-color: ${(props) => props.theme.boardColor};
-  border-radius: 5px;
-  min-height: 100px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  overflow: hidden;
-  grid-column: 1 / 4;
-`;
-
-const Form = styled.form`
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    input {
-        font-size: 16px;
-        border: 0;
-        background-color: white;
-        width: 100%;
-        padding: 10px;
-        border-radius: 5px;
-        text-align: center;
-        margin: 0 auto;
-    }
-`;
-
-interface IForm {
-  board: string;
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+    ]
+  },
+],
+{
+  future: {
+    v7_relativeSplatPath: true,
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_skipActionErrorRevalidation: true,
+  }
+});
 
 function App() {
-  const [toDos, setToDos] = useRecoilState(toDoState);
-
-  const { register, setValue, handleSubmit } = useForm<IForm>();
-  const onValid = ({board}: IForm) => {
-    const newBoard = {
-      id: Date.now(),
-      text: board,
-      cards: [],
-    };
-    setToDos(allBoards => {
-      return [...allBoards, newBoard];
-    });
-    setValue("board", "");
-  };
-  
   return (
-    <Wrapper>
-      <Layer>
-        <Board>
-          <Form onSubmit={handleSubmit(onValid)}>
-            <input
-                {...register("board", { required: true })}
-                type="text" 
-                placeholder="+"
-            />
-          </Form>
-        </Board>
-        {
-          toDos.map(board => <DroppableBoard key={board.id} toDos={board.cards} setToDos={setToDos} boardId={board.id} boardText={board.text} allBoards={board} />)
-        }
-      </Layer>
-    </Wrapper>
+    <>
+      <RouterProvider router={router} future={{ v7_startTransition: true }} />
+    </>
   );
 }
 
---
+++ src/routes/Home.tsx
@@ -0,0 +1,93 @@
+import styled from "styled-components";
+import DroppableBoard from "../components/DroppableBoard";
+import { useRecoilState } from "recoil";
+import { toDoState } from "../atoms";
+import { useForm } from "react-hook-form";
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 480px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Layer = styled.div`
+  display: grid;
+  width: 100%;
+  gap: 10px;
+  grid-template-columns: repeat(3, 1fr);
+`;
+
+const Board = styled.div`
+  padding: 20px 10px;
+  background-color: ${(props) => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  overflow: hidden;
+  grid-column: 1 / 4;
+`;
+
+const Form = styled.form`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    input {
+        font-size: 16px;
+        border: 0;
+        background-color: white;
+        width: 100%;
+        padding: 10px;
+        border-radius: 5px;
+        text-align: center;
+        margin: 0 auto;
+    }
+`;
+
+interface IForm {
+  board: string;
+}
+
+function Home() {
+  const [toDos, setToDos] = useRecoilState(toDoState);
+
+  const { register, setValue, handleSubmit } = useForm<IForm>();
+  const onValid = ({board}: IForm) => {
+    const newBoard = {
+      id: Date.now(),
+      text: board,
+      cards: [],
+    };
+    setToDos(allBoards => {
+      return [...allBoards, newBoard];
+    });
+    setValue("board", "");
+  };
+  
+  return (
+    <Wrapper>
+      <Layer>
+        <Board>
+          <Form onSubmit={handleSubmit(onValid)}>
+            <input
+                {...register("board", { required: true })}
+                type="text" 
+                placeholder="+"
+            />
+          </Form>
+        </Board>
+        {
+          toDos.map(board => <DroppableBoard key={board.id} toDos={board.cards} setToDos={setToDos} boardId={board.id} boardText={board.text} allBoards={board} />)
+        }
+      </Layer>
+    </Wrapper>
+  );
+}
+
+export default Home;
